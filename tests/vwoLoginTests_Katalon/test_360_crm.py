811cc36d7e4524409f95536b232d5895f572ca42
NiteshKumarJordan <ntshkumar365@gmail.com>
2024-11-06 21:18:34 +0530

    06/11/2024

--- tests/vwoLoginTests_Katalon/test_360_crm.py
+++ tests/vwoLoginTests_Katalon/test_360_crm.py
@@ -1,24 +1,17 @@
-
 from tests.pageObjects.Crm_Login import Login_Page_Crm
 import pytest
+import allure_pytest
+import allure
 
 
 class crm_login:
     @pytest.mark.usefixtures("setup")
+    @allure.epic("vwo login Test")
+    @allure.feature("Login page;-negative case")
     def test_crm_login(self, setup):
-        driver =setup
+        driver = setup
         driver.get(self.crm_url)
         crm_l = Login_Page_Crm(driver)
         crm_l.login_to_crm(username="self.username", password="self.password")
         assert "CRM" in driver.title
         assert "Welcome to CRM" in driver.page_source
-
-
-
-
-
-
-
-
-
-
